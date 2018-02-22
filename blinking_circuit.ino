b7c71b22fa602398406e20c1d92233482fcd5636
MahmoudAmrAliAli <36540496+MahmoudAmrAliAli@users.noreply.github.com>
2018-02-22 17:53:51 +0200

    Add files via upload

---
+++ blinking_circuit.ino
@@ -0,0 +1,21 @@
+const int LED =13;
+const int Pushbutton =2;
+int val;
+void setup()
+{
+pinMode(LED,OUTPUT);
+pinMode(Pushbutton,INPUT);  
+}
+void loop()
+{
+  val= digitalRead(Pushbutton);
+if(val ==HIGH)
+{
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000); 
+}
+else { digitalWrite(LED,LOW);}
+}
+
